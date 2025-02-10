3cfc2edeb2213055d635f3df00f5961548a5d816
Monisha-S-21 <211501051@rajalakshmi.edu.in>
2025-02-10 21:24:41 +0530

    Modified

--- TimestampPrinter.java
+++ TimestampPrinter.java
@@ -5,6 +5,6 @@ public class TimestampPrinter {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date = new Date();
-        System.out.println("Current Timestamp: " + formatter.format(date));
+        System.out.println("Current Timestamp- " + formatter.format(date));
     }
 }
